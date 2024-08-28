981d3cc5f513deef2812b8061bde35bca7555e11
Raul Florentino <raul.foliveira12@gmail.com>
2024-08-28 05:21:05 -0300

    Adicionando barra de pesquisa

--- lista-to-do/toDo/src/App.jsx
+++ lista-to-do/toDo/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Todo from './components/Todo'
 import Form from './components/form'
 import './App.css'
+import Search from './components/search'
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -23,7 +25,23 @@ function App() {
       category: "Estudos",
       isCompleted: false,
     },
+    {
+      id:4,
+      text: "Beba água",
+      category: "Passatempo",
+      isCompleted: false,
+    },
+    {
+      id:5,
+      text: "Ler um capitulo",
+      category: "Estudos",
+      isCompleted: false,
+    },
   ]);
+
+
+
+  const [ search, setSearch ] = useState("");
 
   const addTodo = (text, category) => {
     const newTodos = [...todos,
@@ -38,14 +56,37 @@ function App() {
     setTodos(newTodos)
   };
 
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter((todo) => todo.id !== id ? todo : null
+  );
+    setTodos(filteredTodos);
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos);
+  }
 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search 
+        search={search} 
+        setSearch={setSearch}
+      />
       <div className="todo-list">
-        {todos.map((todo) => (
-          //atributo key serve para evitar repetição no REACT, utilizando o ID pois o mesmo não se repete, diferente de text, category
-          <Todo key={todo.id} todo={todo}/>
+        {todos.filter((todo) => 
+          todo.text.toLowerCase().includes(search.toLowerCase())).map(( todo ) => (
+          //atributo key serve para evitar repetição no REACT
+          //utilizando o ID pois o mesmo não se repete, diferente de text, category
+          <Todo 
+            key={todo.id} 
+            todo={todo} 
+            removeTodo={removeTodo} 
+            completeTodo={completeTodo}
+          />
         ))}
       </div>
         <Form addTodo={addTodo}/>
--- lista-to-do/toDo/src/components/todo.jsx
+++ lista-to-do/toDo/src/components/todo.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import '../App.css'; 
 // Certifique-se de que o caminho do arquivo CSS esteja correto
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="todo">
+    <div 
+      className="todo" 
+      style={{ textDecoration: todo.isCompleted ? "line-through" : ""}}
+    >
       <div className="text-container">
         <p>{todo.text}</p>
-        <p className="category">{todo.category}</p>
+        <p className="category">({todo.category})</p>
       </div>
+     
       <div className="button-container">
-        <button className='complete'>Completar</button>
-        <button className='remove'>x</button>
+        <button className='complete' onClick={() =>   completeTodo(todo.id)}>
+          Completar
+        </button>
+        
+        <button className='remove' onClick={() => removeTodo(todo.id)}>
+          x
+        </button>
       </div>
     </div>
   );
